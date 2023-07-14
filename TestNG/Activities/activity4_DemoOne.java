110d004595f400ba7ccd9f4592ba393dfcecd7c0
Krishnakumar Ravi <137033656+Krishnakumar-Ravi-IBM@users.noreply.github.com>
2023-07-14 06:55:57 +0530

    Add files via upload

---
+++ TestNG/Activities/activity4_DemoOne.java
@@ -0,0 +1,14 @@
+package Activity4_suiteExample;
+import org.testng.annotations.Test;
+
+public class activity4_DemoOne {
+    @Test
+    public void firstTestCase() {
+        System.out.println("This is demoOne Class, First Test case");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("This is demoOne Class, Second Test case");
+    }
+}
---
+++ TestNG/Activities/activity4_DemoTwo.java
@@ -0,0 +1,9 @@
+package Activity4_suiteExample;
+import org.testng.annotations.Test;
+
+public class activity4_DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("This is test case in DemoTwo Class,");
+    }
+}
